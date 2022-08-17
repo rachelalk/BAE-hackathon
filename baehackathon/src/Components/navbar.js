33b2d0aa32a026cc15691b053ddf81de2ac75d77
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-17 13:36:33 +0100

    navbar component added

---
+++ baehackathon/src/Components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <h1>Green Steps</h1>
+        </div>
+    )
+}
+
+export default NavBar
