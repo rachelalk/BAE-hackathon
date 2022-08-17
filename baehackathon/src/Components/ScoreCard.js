53d83821d20c5b25fb1d6a7b66b890405b18826e
Your Name~ <you@example.com>
2022-08-17 14:07:45 +0100

    the score board created styling needed

---
+++ baehackathon/src/Components/ScoreCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ScoreCard() {
+  return (
+      <div>
+          <div>
+              <h4>755</h4>
+              <h4>you're doing great!</h4>
+          </div>
+          <h4>499</h4>
+          <h4>499</h4>
+          
+     </div>
+  )
+}
+
+export default ScoreCard
