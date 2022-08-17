10d943236a906193ee74ceade2d35f59d7e29fef
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-17 14:33:49 +0100

    carousel added to app.js

--- baehackathon/src/App.js
+++ baehackathon/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import ScoreCard from './Components/ScoreCard';
 import NavBar from './Components/navbar';
+import Carousel from "./Components/carousel";
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <ScoreCard/>
-    
+        <Carousel/>  
 
     </div>
   );
