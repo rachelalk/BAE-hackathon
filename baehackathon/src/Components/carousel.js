faa6fbd09d88d7142ce9f290f3562e2c3ade9904
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-17 14:44:06 +0100

    plan update

--- baehackathon/src/Components/carousel.js
+++ baehackathon/src/Components/carousel.js
@@ -1,59 +1,59 @@
-import React from "react";
+// import React from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
+// import "swiper/css";
 
-const Carousel = () => {
-	return (
-		<Swiper
-			className="carousel"
-			spaceBetween={10}
-			slidesPerView={1.3}
-			onSlideChange={() => console.log("slide change")}
-			onSwiper={(swiper) => console.log(swiper)}
-		>
-			<SwiperSlide className="slide">
-				<div className="swiper-box">
-					<div className="swiper-image">
-						<img
-							className="carousel-image"
-							alt="light through trees"
-							src="./insights.jpg"
-						/>
-					</div>
-					<div className="swiper-margin"></div>
-					<h3 className="swiper-text"> Insights </h3>
-				</div>
-			</SwiperSlide>
-			<SwiperSlide className="slide">
-				<div className="swiper-box">
-					<div className="swiper-image">
-						<img
-							className="carousel-image"
-							alt="light through trees"
-							src="./insights.jpg"
-						/>
-					</div>
-					<div className="swiper-margin"></div>
-					<h3 className="swiper-text"> Insights </h3>
-				</div>
-			</SwiperSlide>
-			<SwiperSlide className="slide">
-				<div className="swiper-box">
-					<div className="swiper-image">
-						<img
-							className="carousel-image"
-							alt="light through trees"
-							src="./insights.jpg"
-						/>
-					</div>
-					<div className="swiper-margin"></div>
-					<h3 className="swiper-text"> Insights </h3>
-				</div>
-			</SwiperSlide>
-		</Swiper>
-	);
-};
+// const Carousel = () => {
+// 	return (
+// 		<Swiper
+// 			className="carousel"
+// 			spaceBetween={10}
+// 			slidesPerView={1.3}
+// 			onSlideChange={() => console.log("slide change")}
+// 			onSwiper={(swiper) => console.log(swiper)}
+// 		>
+// 			<SwiperSlide className="slide">
+// 				<div className="swiper-box">
+// 					<div className="swiper-image">
+// 						<img
+// 							className="carousel-image"
+// 							alt="light through trees"
+// 							src="./insights.jpg"
+// 						/>
+// 					</div>
+// 					<div className="swiper-margin"></div>
+// 					<h3 className="swiper-text"> Insights </h3>
+// 				</div>
+// 			</SwiperSlide>
+// 			<SwiperSlide className="slide">
+// 				<div className="swiper-box">
+// 					<div className="swiper-image">
+// 						<img
+// 							className="carousel-image"
+// 							alt="light through trees"
+// 							src="./insights.jpg"
+// 						/>
+// 					</div>
+// 					<div className="swiper-margin"></div>
+// 					<h3 className="swiper-text"> Insights </h3>
+// 				</div>
+// 			</SwiperSlide>
+// 			<SwiperSlide className="slide">
+// 				<div className="swiper-box">
+// 					<div className="swiper-image">
+// 						<img
+// 							className="carousel-image"
+// 							alt="light through trees"
+// 							src="./insights.jpg"
+// 						/>
+// 					</div>
+// 					<div className="swiper-margin"></div>
+// 					<h3 className="swiper-text"> Insights </h3>
+// 				</div>
+// 			</SwiperSlide>
+// 		</Swiper>
+// 	);
+// };
 
-export default Carousel;
+// export default Carousel;
